672572fe97e7c9ac7614a8e74e3fd118cc46c469
elyasa9833 <aelyasa09@gmail.com>
2023-09-07 14:44:18 +0700

    commit pertama

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Show todo Dashboard
+Route::get('/', 'TodoController@index');
+
+// show create todo form
+Route::get('/todos/create', 'TodoController@create');
+
+// Add Todo
+Route::post('/todos', 'TodoController@store');
+
+// Show edit todo
+Route::get('/todos/{todo}/edit', 'TodoController@edit');
+
+// update todo
+Route::put('/todos/{todo}', 'TodoController@update');
+
+// delete todo
+Route::delete('/todos/{todo}/delete', 'TodoController@delete');
