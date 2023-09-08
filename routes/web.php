83cda40e87adabeb80e46670ed5fe3b321426f32
elyasa9833 <aelyasa09@gmail.com>
2023-09-08 21:08:26 +0700

    perbaiki controller, view, dan route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,19 +15,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Show todo Dashboard
-Route::get('/', 'TodoController@index');
+Route::get('/', [TodoController::class, 'index']);
+// [CategoryController::class, 'create']
 
 // show create todo form
-Route::get('/todos/create', 'TodoController@create');
+Route::get('/todos/create', [TodoController::class, 'create']);
 
 // Add Todo
-Route::post('/todos','TodoController@store');
+Route::post('/todos',[TodoController::class, 'store']);
 
 // Show edit todo
-Route::get('/todos/{todo}/edit', 'TodoController@edit');
+Route::get('/todos/{todo}/edit', [TodoController::class, 'edit']);
 
 // update todo
-Route::put('/todos/{todo}', 'TodoController@update');
+Route::put('/todos/{todo}', [TodoController::class, 'update']);
 
 // delete todo
-Route::get('/todos/{todo}/delete', 'TodoController@delete');
+Route::get('/todos/{todo}/delete', [TodoController::class, 'delete']);
